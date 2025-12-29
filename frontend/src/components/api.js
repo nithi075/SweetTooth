381652deafbe3496814b38319deaf37402013c63
Sam Nithish <nithishraj8055@gmail.com>
2025-12-29 12:06:15 +0530

    Increase API timeout from 20s to 50s

--- frontend/src/components/api.js
+++ frontend/src/components/api.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 const API = axios.create({
   baseURL: "https://sweettooth-backend.onrender.com/api",
-  timeout: 20000, // 🔥 MUST (20 seconds)
+  timeout: 50000, // 🔥 MUST (20 seconds)
 });
 
 export default API;
